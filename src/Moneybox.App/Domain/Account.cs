d5b96fcfad6c76bb031042d1679d2b71621c917b
rupendrads <50870462+rupendrads@users.noreply.github.com>
2019-05-24 18:28:04 +0100

    Add files via upload

--- src/Moneybox.App/Domain/Account.cs
+++ src/Moneybox.App/Domain/Account.cs
@@ -15,5 +15,30 @@ namespace Moneybox.App
         public decimal Withdrawn { get; set; }
 
         public decimal PaidIn { get; set; }
+
+
+        public void Withdraw(decimal amount)
+        {
+            var fromBalance = Balance - amount;
+            if (fromBalance < 0m)
+            {
+                throw new InvalidOperationException("Insufficient funds to make transfer");
+            }
+
+            Balance -= amount;
+            Withdrawn -= amount;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            var paidIn = PaidIn + amount;
+            if (paidIn > PayInLimit)
+            {
+                throw new InvalidOperationException("Account pay in limit reached");
+            }
+
+            Balance += amount;
+            PaidIn += paidIn;
+        }
     }
 }
